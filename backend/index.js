45ecc6873b05e8ec7eb91293383de3474695e37e
Parmilan <parmilancs2024@gmail.com>
2023-12-31 17:07:13 +0530

    setup some packages

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,23 @@ import colors from 'colors'
 import mongoose from 'mongoose'
 import { PORT, MONGODB_URL } from './Config.js'
 import bookRoute from './routes/bookRoute.js'
+import cors from 'cors'
 
 // Initialize the express App
 const app = express()
 
-// Middleware
+// Middleware for request body
 app.use(express.json())
+
+// middleware for cors policy
+app.use(cors())
+
+// Allow the custom origin
+// app.use({
+//   origin: 'http://localhost:3000',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowHeaders: ['content-type'],
+// })
 
 // Sample APT configuration
 app.get('/', (req, res) => {
