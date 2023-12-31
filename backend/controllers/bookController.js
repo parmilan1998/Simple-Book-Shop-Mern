340a769f80a1deef7c9878fb07185bd895fd6c08
Parmilan <parmilancs2024@gmail.com>
2023-12-31 14:27:41 +0530

    Backend route and controller work update, delete and get products

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,6 +1,7 @@
 import Book from '../models/bookModel.js'
 
-const createBooks = async (req, res) => {
+// Create a new Book
+export const createBooks = async (req, res) => {
   try {
     // Check the required fields
     const { title, author, publishedYear } = req.body
@@ -16,14 +17,87 @@ const createBooks = async (req, res) => {
     }
 
     // Create Book Model
-    const book = await Book.create(newBook)
+    const books = await Book.create(newBook)
 
     // Book created successful response
-    return res.status(201).json(book)
+    return res.status(201).json(books)
   } catch (error) {
-    console.error(error.message)
-    return res.status(500).json({ message: 'Internal Server Error' })
+    console.log(error.message)
+    return res.status(500).send({ message: error.message })
   }
 }
 
-export default createBooks
+// Gets the books
+export const getBooks = async (req, res) => {
+  try {
+    // Find books from mongodb
+    const books = await Book.find({})
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    })
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).send({ message: error.message })
+  }
+}
+
+// Get the books by using id
+export const getBooksById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const book = await Book.findById(id)
+    return res.status(200).json(book)
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).send({ message: error.message })
+  }
+}
+
+// Update book
+export const updateBook = async (req, res) => {
+  try {
+    //  Check if all required fields are provided
+    const { title, author, publishedYear } = req.body
+    if (!title || !author || !publishedYear) {
+      return res.status(400).send({ message: 'Send all the required fields' })
+    }
+
+    const { id } = req.params
+
+    //Find and update the book
+    const result = await Book.findByIdAndUpdate(id, req.body)
+
+    // Check
+    if (!result) {
+      return res.status(404).json({ message: 'Book not found' })
+    }
+
+    // Update Successfully
+    return res.status(200).json({ message: 'Book Update Successfully' })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ message: error.message })
+  }
+}
+
+// Delete a book
+export const deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const result = await Book.findByIdAndDelete(id)
+
+    // Check the book
+    if (!result) {
+      return res.status(404).json({
+        message: 'Book not found',
+      })
+    }
+    // Delete the book message
+    return res.status(200).send({ message: 'Book Deleted Successfully' })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ message: error.message })
+  }
+}
--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -1,8 +1,18 @@
 import { Router } from 'express'
-import createBooks from '../controllers/bookController.js'
+import {
+  createBooks,
+  getBooks,
+  getBooksById,
+  updateBook,
+  deleteBook,
+} from '../controllers/bookController.js'
 
 const router = Router()
 
 router.route('/').post(createBooks)
+router.route('/').get(getBooks)
+router.route('/:id').get(getBooksById)
+router.route('/:id').put(updateBook)
+router.route('/:id').delete(deleteBook)
 
 export default router
